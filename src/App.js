f184bacf731d295b1627f6e6e33657977ce3fc71
BaTrung04 <batrung0410job@gmail.com>
2023-12-24 14:26:18 +0700

    scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import './App.css';
+import './App.scss';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <button className="btn"> add</button>
+            <h1>adsdf</h1>
+        </div>
+    );
 }
 
 export default App;
